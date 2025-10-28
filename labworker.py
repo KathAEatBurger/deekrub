96b2dd75b8c507e70db48354dada0a8ef5186f85
Siraphob Atamakoolsri <khunpokpong@gmail.com>
2025-10-28 20:08:20 +0700

    link employees database from supabase and  make employee details viewable showing employee department

--- labworker.py
+++ labworker.py
@@ -1,20 +1,22 @@
 from flask import Blueprint, render_template
+from supabase_client import get_employees
 
 labworker_bp = Blueprint('labworker', __name__, url_prefix='/labworker')
 
-labworkers = {
-    'Alice': {'name': 'Alice', 'role': 'Chemist', 'email': 'alice@lab.com'},
-    'Bob': {'name': 'Bob', 'role': 'Technician', 'email': 'bob@lab.com'},
-    'Charlie': {'name': 'Charlie', 'role': 'Analyst', 'email': 'charlie@lab.com'}
-}
+labworkers = get_employees()
 
 @labworker_bp.route('/')
 def labworker_home():
-    return render_template('labworker.html', workers=labworkers.values())
+    print("Employees from DB:", labworkers)
+    return render_template('labworker.html', workers=labworkers)
 
 @labworker_bp.route('/<name>')
 def show_labworker(name):
-    worker = labworkers.get(name)
-    if not worker:
-        return "ไม่พบเจ้าหน้าที่คนนี้", 404
-    return render_template('labworker_detail.html', worker=worker)
+    for worker in labworkers:
+        print(name)      
+        print(worker.get("name",name))
+        print(worker)  
+        if worker.get("name",name) == name:
+            return render_template('labworker_detail.html', worker=worker)
+            
+    return "ไม่พบเจ้าหน้าที่คนนี้", 404    
